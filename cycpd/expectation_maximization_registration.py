18c99671f58809b651f0df63b88537894159bb9a
Anthony <anthony@neuralseg.com>
2022-06-30 00:54:11 -0700

    updating format of commit

--- cycpd/expectation_maximization_registration.py
+++ cycpd/expectation_maximization_registration.py
@@ -1,9 +1,8 @@
 import time
 
+import cython_functions as cy
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
-
-import cython_functions as cy
 
 
 class expectation_maximization_registration(object):
