c3f277946c5bdab91da134e08ca11e9bc5d96346
Anthony Gatt <anthony@neuralseg.com>
2022-07-30 22:13:00 -0700

    Bump version to push to pypi - particularly for new readme on pypi

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 
 setup(
     name="cycpd",
-    version="0.24",
+    version="0.25",
     description="Numpy + Cython Implementation of the Coherent Point Drift Algorithm",
     long_description=readme(),
     long_description_content_type="text/markdown",
